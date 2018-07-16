89443abc8361b3342630e1ae900fdb85e8ef2f57
sampsonmao <sampson.mao@gmail.com>
2018-07-16 09:18:39 -0700

    start to add object to control game

--- script.js
+++ script.js
@@ -1,27 +1,40 @@
 const Player = function (name, color) {
     const markBox = function(e) {
-         e.target.style.backgroundColor = color /*player symbol*/
+         e.target.style.backgroundColor = color
          GameBoard.board[e.target.dataset.gridnum] = name
     }
-    return {}
+    return {markBox}
 }
 
+//Initalize gameboard
 const GameBoard = (function() {
     let board = new Array(9)
     //Creates 9x9 grid
+    let gameboard
     for (let i = 0; i<9; i++) {
-        let gameboard = document.getElementById("game-board")
-        let panel = document.createElement("button")
+        gameboard = document.getElementById("game-board")
+        panel = document.createElement("button")
         panel.setAttribute('data-gridnum', i)
         panel.addEventListener("click", Player.markBox)
         gameboard.appendChild(panel)
     }
-    return{board}  
+    return{board, gameboard}  
 })()
 
-const PlayGame = function() {
-    
+let playerOne, playerTwo
+const makePlayers = function() {
+    let playerOneName = document.getElementById('player-one').value
+    let playerTwoName = document.getElementById('player-two').value
+    playerOne = Player(playerOneName, 'blue')
+    playerTwo = Player(playerTwoName, 'purple')
+}
+
+const playGame = function(playerOne, playerTwo) {
+
 }
 
 
+//create player with factory from DOM
+//call player.markbox on their turn
+//create object that alternates turns
 
